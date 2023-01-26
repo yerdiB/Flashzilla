52a823221decd14c62801d8b1d5d2d2a13c37ed5
yerdi <yerdi.isman@gmail.com>
2023-01-26 22:21:30 +0600

    first commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Yerdaulet Ismanaliyev on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
